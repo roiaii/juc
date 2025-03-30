457cffc1aa51b25e349de340da111d79078c726d
lijun <m15239228210@163.com>
2025-03-30 19:06:20 +0800

    add

--- src/main/java/com/TestClassStudent.java
+++ src/main/java/com/TestClassStudent.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class TestClassStudent {
@@ -36,4 +38,62 @@ public class TestClassStudent {
     public int hashCode() {
         return Objects.hash(id, name);
     }
+
+
+    public static void main(String[] args) {
+        TestClassStudent testClassStudent1 = new TestClassStudent();
+        testClassStudent1.setId(1l);
+        testClassStudent1.setName("lijun");
+
+        TestClassStudent testClassStudent2 = new TestClassStudent();
+        testClassStudent2.setId(1l);
+        testClassStudent2.setName("lijun");
+
+        Student student1 = new Student();
+        student1.setId(1l);
+        student1.setName("lijun");
+
+        Student student2 = new Student();
+        student2.setId(1l);
+        student2.setName("lijun");
+
+        // 两个对象内容相同
+        System.out.println(student1.equals(student2)); // false，没有重写，比较的是指针
+        System.out.println(testClassStudent1.equals(testClassStudent2)); // true，重写，比较对象的内容
+
+        // 测试hashmap put元素的过程，首先计算hashcode，然后判断是否发生哈希冲突，调用equals方法判是否是同一个元素
+        HashMap<TestClassStudent, String> map1 = new HashMap<>();
+        HashMap<Student, String> map2 = new HashMap<>();
+
+        map1.put(testClassStudent1, testClassStudent1.getName());
+        System.out.println(map1.containsKey(testClassStudent2)); // true，两者hashcode以及equals是一样的
+        map1.put(testClassStudent2, testClassStudent2.getName());
+        System.out.println(map1.size() + ":" + map1.get(testClassStudent1)); // 1 : lijun
+
+        map2.put(student1, student1.getName());
+        System.out.println(map2.containsKey(student2)); // false，两者hashcode以及equals是不同的
+    }
+}
+
+
+class Student {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -2,20 +2,57 @@ package com.test;
 
 import org.springframework.context.annotation.Conditional;
 
-import java.security.KeyStore;
-import java.sql.SQLOutput;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Test {
-
     public static void main(String[] args) {
-        HashMap<Integer, String> h = new HashMap<>();
-        h.put(1, "2");
+        n = 2133;
+        int[] nums = new int[]{1, 2, 3, 9};
+        used = new boolean[nums.length];
+        len = 4;
+        dfs(nums);
+        System.out.println(ans);
+    }
 
+    static int ans = 0;
+    static int len = 0;
+    static int n = 0;
+    static boolean[] used;
+    static ArrayList<Integer> path = new ArrayList<>();
+
+
+    /**
+     * 从nums数组进行元素组合，可以从重复选，寻找不大于n的最大的数
+     * @param nums
+     */
+    private static void dfs(int[] nums) {
+        if (path.size() > len) {
+            return;
+        }
+
+        int t = calNum(path);
+
+        if (t > ans && t < n) {
+            ans = t;
+        }
+
+        for (int i=0; i<nums.length; i++) {
+            path.add(nums[i]);
+            dfs(nums);
+            path.remove(path.size()-1);
+        }
+    }
+
+    private static int calNum(ArrayList<Integer> lists) {
+        int ans = 0;
+        int ff = 0;
+
+        for (int i= lists.size()-1; i>=0; i--) {
+            ans += (lists.get(i) * Math.pow(10, ff++));
+        }
+
+        return ans;
 
     }
 }
+
